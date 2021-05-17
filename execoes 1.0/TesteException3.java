132fcda6e4722349ede4d8fc84f826e1ef151f6a
Lucas-ol45 <70076035+Lucas-ol45@users.noreply.github.com>
2021-05-16 22:02:57 -0300

    Add files via upload

---
+++ execoes 1.0/TesteException3.java
@@ -0,0 +1,18 @@
+package execoes;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TesteException3 {
+	public static void main(String[] args) {
+		
+	//	public static InputStreamReader conn;
+		
+		try {
+			//new FileReader("C:\\Arquivodeteste.txt");
+		} finally {
+		//conn.close(); 
+		}
+	}
+}
