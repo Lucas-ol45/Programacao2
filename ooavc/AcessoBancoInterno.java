bb02d15948644cd2ff7536d1fc4642d694099c88
Lucas-ol45 <70076035+Lucas-ol45@users.noreply.github.com>
2021-05-16 21:50:42 -0300

    Add files via upload

---
+++ ooavc/AcessoBancoInterno.java
@@ -0,0 +1,9 @@
+package ooavc;
+
+interface AcessoBancoInterno {
+	abstract boolean verifica(int senha);
+
+	
+	
+	
+}
---
+++ ooavc/TestaConta5.java
@@ -0,0 +1,21 @@
+package ooavc;
+
+public class TestaConta5 {
+
+	public static void main(String[] args) {
+		
+		Conta3 c3 = new Conta3(003,"mateus",100,100);
+		System.out.println(c3.cliente.nome);
+		System.out.println(c3.getcontadorContas());
+		
+		Conta3 c4 = new Conta3(003,"Rodrigo",100,100);
+		System.out.println(c4.cliente.nome);
+		System.out.println(c4.getcontadorContas());
+		
+		Conta3 c5 = new Conta3(003,"Luiza",100,100);
+		System.out.println(c5.cliente.nome);
+		System.out.println(c5.getcontadorContas());
+		
+	}
+	
+}
